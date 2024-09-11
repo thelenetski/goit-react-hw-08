6ee703cea66b0a681c201e7b0e5dcc3b7b32dc67
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-09-11 10:39:03 +0300

    finish modal

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,59 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import css from './Contact.module.css';
 import { FaPhone } from 'react-icons/fa6';
 import { FaUser } from 'react-icons/fa6';
 import { IconButton } from '@mui/material';
 import { MdEdit } from 'react-icons/md';
 import { IoClose } from 'react-icons/io5';
-import { openConfirmDelete, openEditContact } from '../../redux/modal/slice';
+import {
+  modalTypes,
+  openConfirmDelete,
+  openEditContact,
+} from '../../redux/modal/slice';
 import ModalWindow from '../Modal/Modal';
+import {
+  selectContentModal,
+  selectTypeModal,
+} from '../../redux/modal/selectors';
+import { useEffect, useState } from 'react';
+import { deleteContact, patchContact } from '../../redux/contacts/operations';
+import EditForm from '../Modal/EditForm';
 
 const Contact = ({ contact }) => {
   const dispatch = useDispatch();
+  const type = useSelector(selectTypeModal);
+  const content = useSelector(selectContentModal);
+
+  const [editedContact, setEditedContact] = useState({
+    name: '',
+    number: '',
+  });
+
+  useEffect(() => {
+    if (type !== modalTypes.editContact) return;
+    setEditedContact(content);
+  }, [content, type]);
+
+  const handleDelete = () => {
+    dispatch(deleteContact(content.id));
+  };
+
+  const handleEditChange = e => {
+    const { name, value } = e.target;
+    setEditedContact(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleEditSubmit = () => {
+    dispatch(patchContact({ ...editedContact }));
+  };
+
+  const onSuccess = () => {
+    if (type === modalTypes.confirmDelete) return handleDelete();
+    if (type === modalTypes.editContact) return handleEditSubmit();
+  };
 
   return (
     <div className={css.contact}>
@@ -39,8 +83,17 @@ const Contact = ({ contact }) => {
       >
         <IoClose />
       </IconButton>
-      <ModalWindow>
-        <p>Are you sure you want to delete this contact?</p>
+      <ModalWindow onSuccess={onSuccess}>
+        {type === modalTypes.editContact && (
+          <EditForm
+            editedContact={editedContact}
+            handleEditChange={handleEditChange}
+          />
+        )}
+
+        {type === modalTypes.confirmDelete && (
+          <p>Are you sure you want to delete this contact?</p>
+        )}
       </ModalWindow>
     </div>
   );
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,52 +4,12 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { useDispatch, useSelector } from 'react-redux';
-import { closeModal, modalTypes } from '../../redux/modal/slice';
-import {
-  selectContentModal,
-  selectIsOpenModal,
-  selectTypeModal,
-} from '../../redux/modal/selectors';
-import { useEffect, useState } from 'react';
-import { deleteContact, patchContact } from '../../redux/contacts/operations';
-import EditForm from './EditForm';
+import { closeModal } from '../../redux/modal/slice';
+import { selectIsOpenModal } from '../../redux/modal/selectors';
 
-const ModalWindow = ({ children }) => {
+const ModalWindow = ({ onSuccess, children }) => {
   const dispatch = useDispatch();
   const isOpen = useSelector(selectIsOpenModal);
-  const type = useSelector(selectTypeModal);
-  const content = useSelector(selectContentModal);
-
-  const [editedContact, setEditedContact] = useState({
-    name: '',
-    number: '',
-  });
-
-  useEffect(() => {
-    if (type !== modalTypes.editContact) return;
-    setEditedContact(content);
-  }, [content, type]);
-
-  const handleDelete = () => {
-    dispatch(deleteContact(content.id));
-  };
-
-  const handleEditChange = e => {
-    const { name, value } = e.target;
-    setEditedContact(prev => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleEditSubmit = () => {
-    dispatch(patchContact({ ...editedContact }));
-  };
-
-  const onSuccess = () => {
-    if (type === modalTypes.confirmDelete) return handleDelete();
-    if (type === modalTypes.editContact) return handleEditSubmit();
-  };
 
   const onClose = () => dispatch(closeModal());
 
@@ -65,13 +25,7 @@ const ModalWindow = ({ children }) => {
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Notification
           </Typography>
-          {type === modalTypes.confirmDelete && children}
-          {type === modalTypes.editContact && (
-            <EditForm
-              editedContact={editedContact}
-              handleEditChange={handleEditChange}
-            />
-          )}
+          {children}
           <Button
             onClick={onSuccess}
             variant="contained"
